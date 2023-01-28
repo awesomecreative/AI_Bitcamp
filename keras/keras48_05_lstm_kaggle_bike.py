2921506b7a0766a535435f38d06c22062f73fea6
awesomecreative <120912398+awesomecreative@users.noreply.github.com>
2023-01-28 11:10:51 +0900

    1/27(금)

--- keras/keras48_05_lstm_kaggle_bike.py
+++ keras/keras48_05_lstm_kaggle_bike.py
@@ -51,7 +51,7 @@ import time
 model.compile(loss='mse', optimizer='adam', metrics=['mae','mse'])
 
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-es = EarlyStopping(monitor='val_loss', mode='min', patience=5, verbose=1, restore_best_weights=True)
+es = EarlyStopping(monitor='val_loss', mode='min', patience=10, verbose=1, restore_best_weights=True)
 
 
 import datetime
@@ -65,7 +65,7 @@ mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, save_best_only
                      filepath = filepath + 'LSTM_bi_' + date + '_' + filename)
 
 start = time.time()
-hist = model.fit(x_train, y_train, epochs=1000, batch_size=32, validation_split=0.2, verbose=1,
+hist = model.fit(x_train, y_train, epochs=100, batch_size=32, validation_split=0.2, verbose=1,
                  callbacks=[es, mcp])
 end = time.time()
 
@@ -107,6 +107,7 @@ submission_csv.to_csv(filepath2 + 'submission_LSTM_' + date + '.csv')
 print('time: ', end-start)
 
 """
+<1>
 Epoch 11/1000
 218/218 [==============================] - ETA: 0s - loss: 32813.3047 - mae: 141.9877 - mse: 32813.3047Restoring model weights from the end of the best epoch: 6.
 
@@ -118,4 +119,32 @@ loss :  [34151.7890625, 145.07809448242188, 34151.7890625]
 RMSE:  184.8020410469438
 r2:  -0.0015149688208162537
 time:  124.28226661682129
+
+<2>
+Epoch 10/1000
+216/218 [============================>.] - ETA: 0s - loss: 32819.0586 - mae: 142.2222 - mse: 32819.0586Restoring model weights from the end of the best epoch: 5.
+
+Epoch 00010: val_loss did not improve from 31364.11719
+218/218 [==============================] - 6s 26ms/step - loss: 32890.7969 - mae: 142.3065 - mse: 32890.7969 - val_loss: 31414.5859 - val_mae: 142.0155 - val_mse: 31414.5859
+Epoch 00010: early stopping
+69/69 [==============================] - 0s 6ms/step - loss: 34176.9609 - mae: 144.8255 - mse: 34176.9609
+loss :  [34176.9609375, 144.82545471191406, 34176.9609375]
+RMSE:  184.87012907134385
+r2:  -0.0022530963582068075
+time:  57.063015937805176
+
+<3>
+Epoch 13/100
+216/218 [============================>.] - ETA: 0s - loss: 32855.8750 - mae: 142.3468 - mse: 32855.8750Restoring model weights from the end of the best epoch: 3.
+
+Epoch 00013: val_loss did not improve from 31373.54102
+218/218 [==============================] - 6s 26ms/step - loss: 32827.7812 - mae: 142.3267 - mse: 32827.7812 - val_loss: 31433.9395 - val_mae: 142.2992 - val_mse: 31433.9395
+Epoch 00013: early stopping
+69/69 [==============================] - 0s 6ms/step - loss: 34125.8398 - mae: 145.4358 - mse: 34125.8398
+loss :  [34125.83984375, 145.435791015625, 34125.83984375]
+RMSE:  184.73180512073813
+r2:  -0.0007538412501422176
+time:  75.15612959861755
+
+결과: kaggle_bike 자료에서 LSTM은 성능이 좋지 않은 것 같다.
 """
